52325dd8eeb2b8acc3e23db08681905a205ab3ef
Quarantining <165849060+Quarantining@users.noreply.github.com>
2024-09-28 05:09:05 -0700

    v1.1 Fixed Logic
    Turns out I did my math wrong because I was dividing the number to early, had gpt help me fix it. I understand the logic better now so this should be working.

--- binary-number-conveter.py
+++ binary-number-conveter.py
@@ -3,12 +3,9 @@ while True:
     binary_num = []
 
     while decimal_num > 0:
-        if decimal_num % 2 == 1:
-            decimal_num //= 2
-            binary_num.append(1)
-        else:
-            decimal_num //= 2
-            binary_num.append(0)
+        remainder = decimal_num % 2
+        binary_num.append(remainder)
+        decimal_num //= 2
 
     binary_num.reverse()
     result = ''.join(str(i) for i in binary_num)
